22c583a76e007338ff732d51fe7a05814554e985
thanhphatsainin <namtc101099@gmail.com>
2021-09-21 08:52:39 +0700

    Choose Date Screen :Create UI, Handle Event, Fetch and display data

--- NTC-Weather-Forecast-COVID-19/NTC-Weather-Forecast-COVID-19/Screen/Covid19/ChooseDate/ChooseDateViewController.swift
+++ NTC-Weather-Forecast-COVID-19/NTC-Weather-Forecast-COVID-19/Screen/Covid19/ChooseDate/ChooseDateViewController.swift
@@ -7,9 +7,44 @@
 
 import UIKit
 
+protocol ChooseDateDelegate: AnyObject {
+    func sendDate(date: Date)
+}
+
 final class ChooseDateViewController: UIViewController {
 
+    @IBOutlet private weak var chooseDatePicker: UIDatePicker!
+    
+    weak var delegate: ChooseDateDelegate?
+    private var chooseDate = Date()
+    var countryCovids = [CountryCovid]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupChooseDatePicker()
+    }
+    
+    private func setupChooseDatePicker() {
+        if !countryCovids.isEmpty {
+            let min = countryCovids.first?.date.subStringTime() ?? ""
+            let max = countryCovids.last?.date.subStringTime() ?? ""
+            chooseDatePicker.setMinMaxTime(min: min, max: max)
+        }
+    }
+    
+    @IBAction
+    private func onChooseDatePicker(_ sender: UIDatePicker) {
+        chooseDate = sender.date
+    }
+    
+    @IBAction
+    private func clickOkButton(_ sender: UIButton) {
+        delegate?.sendDate(date: chooseDate)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction
+    private func clickCancelButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
 }
